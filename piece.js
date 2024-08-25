0ac3615ca6c4a0fe8c55649a11f778b3b64307b1
trungpro200 <dinhtrungnguyen2005@gmail.com>
2024-08-25 20:30:28 +0700

    added hints

--- piece.js
+++ piece.js
@@ -35,6 +35,9 @@ for (var row = 0; row < 8; row++) {
         chessroot.appendChild(piece)
     }
 }
+
+
+
 function updateSelectBox(newPos = "", show=false) {
     var sbox = document.getElementById("select-box")
 
@@ -52,16 +55,33 @@ function updateSelectBox(newPos = "", show=false) {
 
     sbox.style.top = `${parseInt(pos[0]) * cellSize}px`
     sbox.style.left = `${parseInt(pos[1]) * cellSize}px`
+
+    // console.log(chessNot2Int(int2ChessNot(pos)))
 }
 
 /**
- * @param {HTMLElement} element 
+ * @param {HTMLElement} piece 
  */
-function updatePiece(element) { //Update position ONLY
-    var pos = element.id.slice(1)
+function updatePiece(piece) { //Update position ONLY
+    var pos = piece.id.slice(1)
 
-    element.style.top = `${parseInt(pos[0]) * cellSize}px`;
-    element.style.left = `${parseInt(pos[1]) * cellSize}px`;
+    piece.style.top = `${parseInt(pos[0]) * cellSize}px`;
+    piece.style.left = `${parseInt(pos[1]) * cellSize}px`;
+}
+/**
+ * @param {Array<String>} moves
+ */
+function createHints(moves){
+    moves.forEach(function (move){
+        var hint = document.createElement("div")
+        move = chessNot2Int(move)
+        
+        hint.style.top = `${parseInt(move[0])*cellSize}px`
+        hint.style.left = `${parseInt(move[1])*cellSize}px`
+        hint.className = "hint"
+
+        document.getElementById("hints").appendChild(hint)
+    })
 }
 
 function updatePieces() { //Should be called ONLY ONCE
@@ -72,25 +92,62 @@ function updatePieces() { //Should be called ONLY ONCE
             if (!whiteTurn){
                 return
             }
-
-            var targetPiece = event.target;
-            var pos = targetPiece.id.slice(1);
+            clearHints()
+            var pos = this.id.slice(1);
+            
+            if (this.classList[2][0]=='w'){
+                createHints(game.moves(int2ChessNot(pos)))
+            }
 
             updateSelectBox(pos, true)
         })
     })
 }
+
+function clearHints(){
+    document.getElementById("hints").innerHTML=''
+}
+
 /**
- * @param {Number} cel
+ * @param {string} cel
  */
 function int2ChessNot(cel){
-    
+    var lets = "abcdefgh"
+    return lets[parseInt(cel[1])]+String(8-parseInt(cel[0]))
+}
+
+/**
+ * @param {string} not
+ */
+function chessNot2Int(not){
+    not = not.toLowerCase()
+
+    return `${8-not[1]}${not.charCodeAt(0)-97}`
 }
 
 document.addEventListener("click", function (event){
     if (!chessroot.contains(event.target)){
         updateSelectBox()
+        clearHints()
     }
+})
+
+chessroot.addEventListener("click", function (event){
+    if (document.getElementById("select-box").style.visibility=='hidden'){
+        return
+    }
+
+    const rect = this.getBoundingClientRect()
+
+    var x = event.clientX-rect.left
+    var y = event.clientY-rect.top
+    
+    x = Math.floor(x/cellSize)
+    y = Math.floor(y/cellSize)
+
+    var posInt = `${y}${x}`
+    var pos
+    console.log(posInt) 
 })
 
 updatePieces()
