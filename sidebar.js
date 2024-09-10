fc8d079373f7520d5a6c3055411b263982f7076c
trungpro200 <dinhtrungnguyen2005@gmail.com>
2024-09-10 20:06:20 +0700

    Fixed navbtn

--- sidebar.js
+++ sidebar.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.querySelector(".sidebar").classList.add("active");
     document.querySelector("#main").classList.add("shifted");
+    document.querySelector("#navbtn").setAttribute("onclick", "closeNav()")
 }
 
 function closeNav() {
     document.querySelector(".sidebar").classList.remove("active");
     document.querySelector("#main").classList.remove("shifted");
+    document.querySelector("#navbtn").setAttribute("onclick", "openNav()")
 }
